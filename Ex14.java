e2927e50c1bbff333a2a3eb3cedc026dfdec85e0
taisamfr <taisa.reis1a@gmail.com>
2025-03-31 15:03:28 -0300

    algoritimo

---
+++ Ex14.java
@@ -0,0 +1,13 @@
+/*
+Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+*/
+import java.util.Scanner;
+public class Ex14 {
+	public static void main(String[] args) {
+	 Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < 10; i++){
+		    System.out.println("Eu gosto de estudar algoritimo!");
+		}
+	}
+}
