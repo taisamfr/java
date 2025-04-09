f09943994bfd74bc73ea119acdde5ea0f047e881
taisinha <taisa.reis1a@gmail.com>
2025-04-09 07:21:03 -0300

    numero lado

---
+++ Ex31.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+31. Faça um programa que imprima na tela os números de 1 a 20, 
+um abaixo do outro. Depois modifique o programa para que ele mostre
+os números um ao lado do outro.
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex31
+{
+	public static void main(String[] args) {
+	   for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+	   }
+	}
+}
