4366477f84ed10085cd9b458fb562b1fca77e023
taisinha <taisa.reis1a@gmail.com>
2025-04-09 07:24:17 -0300

    DoWhile

---
+++ Ex34.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+34. exemplo de DoWhile
+*******************************************************************************/
+public class Ex34
+{
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		do {
+		    System.out.println(contador);
+		    contador++;
+		} while (contador <= 5);
+	}
+}
