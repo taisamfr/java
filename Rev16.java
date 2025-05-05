a26412e60383ae3e0450e9fd01dfd81ae047a2bc
taisinha <taisa.reis1a@gmail.com>
2025-05-05 09:59:16 -0300

    numero inteiro

---
+++ Rev16.java
@@ -0,0 +1,21 @@
+/*******************************************************************
+ Exercicio 016
+ Faça um Programa que peça um número inteiro 
+ e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+ 
+ ***********************************************************************/
+ 
+ import java.util.Scanner;
+ public class Rev16 {
+      public static void main(String[]args) {
+          Scanner ler = new Scanner(System.in);
+          System.out.println("Informe um número inteiro: ");
+          int numero = ler.nextInt();
+          
+          if (numero % 2 == 0) {
+              System.out.println("Esse número é par!");
+          } else {
+              System.out.println("Esse número é ímpar!");
+          }
+      }
+ }
